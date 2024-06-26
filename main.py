1c7c0e7a1f21ac6fe2554608a4251c8738a0a03d
KsushaZaya <zayakina.oksana@mail.ru>
2024-06-26 22:05:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+a = ('information')
+print(len(a))
+
+first = (10)
+second = (8)
+summa = (first + second)
+diff = (first - second)
+print(summa, diff)
+
+first = (2)
+second = (4)
+third = (6)
+summa = (first + second + third)
+mean = (summa / 3)
+print(int(mean))
+
+first_string = ('Вторник')
+second_string = ('Понедельник,')
+print(second_string, first_string)
+
+a = (2)
+b = (3)
+c = (7)
+f = ((a*b) + (a*c))
+print(f)
+print(int((f ** 3) /2))
+
+
